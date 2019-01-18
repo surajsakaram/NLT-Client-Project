02b85b8ef00b522c3152bc57b9a3d2ec49f86428
Suraj Sakaram <suraj.sakaram@gmail.com>
2019-01-17 18:47:49 -0800

    pickled model and vectorized matrix objects, updated flask server

--- code/webpage.py
+++ code/webpage.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.feature_extraction.text import TfidfVectorizer, ENGLISH_STOP_WORDS
 import ast
+import pickle
 app = Flask(__name__)
 
 
@@ -60,28 +61,42 @@ def json_route():
         json = response.json()['articles']
         data.extend(json)
 
+    # Build dataframe
     df_temp = pd.DataFrame(data)
     df_temp.drop(['author', 'publishedAt', 'source', 'urlToImage'], axis=1, inplace=True)
     df_temp['text'] = df_temp['title'] + df_temp['description'] + df_temp['content']
 
-    vect = TfidfVectorizer(stop_words='english',token_pattern='[a-zA-Z]+',min_df=5)
-    X = vect.fit_transform(df_temp['text'].dropna()).toarray()
-    tfidf = pd.DataFrame(X, columns=vect.get_feature_names())
+    model_dict = {
+        'wildfire': '../pickles/wildfire_2.pk',
+        'earthquake': '../pickles/earthquake_2.pk',
+        'flood' : '../pickles/flood_2.pk'
+    }
 
-    model = pickle.load(open('../data/wildfire.pk', 'rb'))
+    vect_dict = {
+        'wildfire': '../pickles/wildfire_vect.pk',
+        'earthquake': '../pickles/earthquake_vect.pk',
+        'flood': '../pickles/flood_vect.pk'
+    }
 
+    # Load in pickled objects
+    vect = pickle.load(open(vect_dict[search_term], 'rb'))
+    model = pickle.load(open(model_dict[search_term], 'rb'))
+
+    # Transform data using fit vectorized object
+    X = vect.transform(df_temp['text'].dropna()).toarray()
+    tfidf = pd.DataFrame(X, columns = vect.get_feature_names())
+
+    # Tranform vectorized data using LDA and get out relevant documents
     comp = model.transform(tfidf)
-
     document_topics = pd.DataFrame(comp, columns=["topic %d" % i for i in range(comp.shape[1])])
-
-    top_topics = (document_topics['topic 0'] > .2)
+    top_topics = (document_topics['topic 1'] > .3)
     docs = document_topics[top_topics].index
 
     df = df_temp.loc[docs]
     df.drop(['content', 'text'], axis=1, inplace=True)
 
     posts = ast.literal_eval(df.to_json(orient='records'))
-    return render_template('wildfire.html', posts = posts, title='blah')
+    return render_template('disaster.html', posts = posts, title='blah')
 
 
 # Allows us to dynamically update webpage without having to quit the flask server
